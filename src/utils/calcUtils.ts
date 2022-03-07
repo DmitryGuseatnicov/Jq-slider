d36bfc66397d89ec3c5f88bf7b522493bdc773ae
Dmitry Guseatnicov <dimagusi111@gmail.com>
2022-03-07 21:37:36 +0300

    Добавил функции для конвертации данных

--- src/utils/calcUtils.ts
+++ src/utils/calcUtils.ts
@@ -4,4 +4,12 @@ function convertValueInPercent(min: number, max: number, value: number): number 
   return 100 / (max - min) * (value - min);
 }
 
-export { convertValueInPercent };
+function convertPixelInPercent(width: number, value: number): number {
+  return 100 / width * value;
+}
+
+function convertPercentInValue(min: number, max: number, percent: number): number {
+  return (max - min) / 100 * percent + min;
+}
+
+export { convertValueInPercent, convertPixelInPercent, convertPercentInValue };
