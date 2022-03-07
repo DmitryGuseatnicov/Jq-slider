7c90d8e1b070ae47684d137596eb8e34c0c4d18c
Dmitry Guseatnicov <dimagusi111@gmail.com>
2022-03-08 01:18:17 +0300

    Добавил тесты для SecondTip

---
+++ src/AppComponents/View/SubViewComponents/Tip/SecondTip.test.ts
@@ -0,0 +1,64 @@
+/**
+ *  @jest-environment jsdom
+ */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-undef */
+
+import { State } from '../../../../types/types';
+import SecondTip from './SecondTip';
+
+describe('Test Tip', () => {
+  let tip: SecondTip;
+  let slider: HTMLElement;
+  let newState: State;
+
+  beforeEach(() => {
+    slider = document.createElement('div');
+    slider.style.width = '1000';
+    tip = new SecondTip(slider);
+    newState = {
+      min: 0,
+      max: 100,
+      from: 10,
+      to: 90,
+      step: 1,
+      tip: true,
+      range: true,
+      progress: true,
+      scale: true,
+      scaleDestiny: 1,
+      horizontal: false,
+    };
+  });
+
+  test('Should be instance of Tip', () => {
+    expect(tip).toBeInstanceOf(SecondTip);
+  });
+
+  test('Should be correct position on slider aria', () => {
+    newState = {
+      ...newState, min: 0, max: 200, to: 20,
+    };
+    tip.setState(newState);
+    expect(tip.subView.style.left).toBe('10%');
+
+    newState = {
+      ...newState, min: 0, max: 200, to: 10,
+    };
+    tip.setState(newState);
+    expect(tip.subView.style.left).toBe('5%');
+
+    newState = {
+      ...newState, min: -100, max: 100, to: -90,
+    };
+    tip.setState(newState);
+    expect(tip.subView.style.left).toBe('5%');
+
+    newState = {
+      ...newState, min: -100, max: 100, to: 0,
+    };
+    tip.setState(newState);
+    expect(tip.subView.style.left).toBe('50%');
+  });
+});
