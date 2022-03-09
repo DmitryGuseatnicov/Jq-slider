cc8b97ac16044bed586971b3d8f4b26add040114
Dmitry Guseatnicov <dimagusi111@gmail.com>
2022-03-09 15:32:39 +0300

    Написал тесты для Model

---
+++ src/AppComponents/Model/Model.test.ts
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import { State } from '../../types/types';
+import Model from './Model';
+
+describe('Test Model', () => {
+  let model: Model;
+  let state: State;
+
+  beforeEach(() => {
+    state = {
+      min: 0,
+      max: 100,
+      from: 0,
+      to: 100,
+      step: 1,
+      tip: true,
+      range: true,
+      progress: true,
+      scale: true,
+      scaleDestiny: 1,
+      horizontal: false,
+    };
+    model = new Model(state);
+  });
+
+  test('Should be instance of Model', () => {
+    expect(model).toBeInstanceOf(Model);
+  });
+
+  test('Should be correct state after init', () => {
+    expect(model.getState()).toEqual(state);
+  });
+
+  test('Should be correct values after update from or to', () => {
+    model.setState({ from: 10 });
+    expect(model.getState().from).toBe(10);
+
+    model.setState({ from: -10 });
+    expect(model.getState().from).toBe(0);
+
+    model.setState({ from: 43 });
+    expect(model.getState().from).toBe(43);
+
+    model.setState({ to: 90 });
+    expect(model.getState().to).toBe(90);
+
+    model.setState({ to: 1 });
+    expect(model.getState().to).toBe(43);
+
+    model.setState({ to: 87 });
+    expect(model.getState().to).toBe(87);
+  });
+});
